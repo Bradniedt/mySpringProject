a118338777705db483c37711bc5f61987496be84
Bradniedt <bradleyniedt@gmail.com>
2021-08-22 10:51:53 -0600

    Adds comments to controller.

--- src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // Get endpoint for Hello World
     @GetMapping("/helloworld")
     public String helloWorld() {
         return "Hello World!";
     }
+
+    // Get endpoint for Hi World
     @GetMapping("/hiworld")
     public String hiWorld() {
         return "Hi World!";
